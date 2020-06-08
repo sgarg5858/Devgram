5f653668d5efe2df231e3d5d7d6accb6f3bced63
Sanjay <sgarg5858@gmail.com>
2020-06-08 18:08:13 +0530

    Jwt Verify and Logged In User Profile.

---
+++ src/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+module.exports =  function(req,res,next) {
+
+    const token=req.header('x-auth-token');
+    if(!token)
+    {
+        return res.status(400).json({errors:[{msg:"No Authorized Token"}]});
+    }
+    //Verify Token
+    try {
+        //header and payload are just base64 encoded they are not encrypted...
+        //signature is important it is build using payload,header and secret key and then hashed only one way
+        //this works by again generating signature from header and payload if both signature matches then
+        //it is authenticated header.payload.signature
+      const decoded=  jwt.verify(token,'mySecret');
+      req.user=decoded.user;
+      next();
+      
+    } catch (error) {
+        req.status(401).json({errors:[{msg:"Invalid Token"}]})   
+    }
+}
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,7 +4,8 @@ const {check,validationResult} = require('express-validator')
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-
+const auth= require('../middleware/auth');
+//First API Login API
 router.post('/login',[
     check('email','Please Enter Valid Email').isEmail(),
     check('password','Please Enter Valid Password').not().isEmpty()
@@ -50,5 +51,17 @@ router.post('/login',[
     }
 })
 
+//Second API get current logged in User 
+router.get('/me',auth, async (req,res,next)=>{
+
+    try {
+        console.log(req.user.id);
+        const user= await User.findById(req.user.id);
+        return res.json(user);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send('Internal Server Error');
+    }
+})
 
 module.exports=router;
