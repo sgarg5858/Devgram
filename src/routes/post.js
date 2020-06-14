89a66072461702e0f803fb6d951bb7e382b53e40
Sanjay <sgarg5858@gmail.com>
2020-06-14 18:45:12 +0530

    Post api's for creating,deleting,liking,disliking comments are completed

--- src/routes/post.js
+++ src/routes/post.js
@@ -130,4 +130,123 @@ router.put('/unlike/:postid',auth,async(req,res,next)=>{
         return res.status(500).json('Server Error');
     }
 })
+//Adding Comment to Post
+router.post('/addComment/:postid',[auth,[
+    check('text','text is required').not().isEmpty()
+]], async(req,res,next)=>{
+
+    const errors= validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({msg:'Enter Some Text'});
+    }
+    try {
+        const user =await User.findById(req.user.id);
+        const post =await Post.findById(req.params.postid);
+        const newComment={
+            text:req.body.text,
+            user:req.user.id,
+            name:user.name,
+            avatar:user.avatar
+        };
+        post.comments.unshift(newComment);
+        await post.save();
+        return res.json(post.comments);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json('Server Error');
+    }
+})
+//Delete Comment
+
+router.delete('/deleteComment/:postid/:commentid',auth, async(req,res,next)=>{
+
+    try {
+        // const user =await User.findById(req.user.id);
+        const post =await Post.findById(req.params.postid);
+      
+        const comment =  post.comments.find(comment=> comment._id.toString()===req.params.commentid);
+
+        if(!comment)
+        {
+            return res.status(404).json("Comment does not exist");
+        }
+        //User who created comment is deleting comment
+        if(comment.user.toString()!==req.user.id)
+        {
+            return res.status(400).json({msg:"User Not Authorized"});
+        }
+        const removeIndex=post.comments.map(comment=>comment._id.toString()).indexOf(req.params.comment_id);
+        post.comments.splice(removeIndex,1);
+        await post.save();
+        return res.json(post.comments);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json('Server Error');
+    }
+})
+//Like Comment
+router.put('/likeComment/:postid/:commentid',auth, async(req,res,next)=>{
+    try {
+
+        const post = await Post.findById(req.params.postid);
+        if(!post)
+        {
+            return res.status(404).json("Post not Found");
+        }
+
+        const comment = post.comments.find((comment)=>{return comment._id.toString()=== req.params.commentid});
+        if(!comment)
+        {
+            return res.status(404).json("Comment not Found");
+        }
+
+        if(comment.likes.filter((like)=>{return like.user.toString()===req.user.id}).length > 0)
+        {
+            return res.status(404).json("Comment Already Liked");
+        }
+        const user= await User.findById(req.user.id);
+
+        comment.likes.unshift({
+            user:req.user.id,
+            name:user.name
+        });
+        console.log(comment);
+        post.save();
+        return res.json(comment.likes);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json('Server Error');
+    }
+})
+//DisLike Comment 
+router.put('/dislikeComment/:postid/:commentid',auth, async(req,res,next)=>{
+    try {
+
+        const post = await Post.findById(req.params.postid);
+        if(!post)
+        {
+            return res.status(404).json("Post not Found");
+        }
+
+        const comment = post.comments.find((comment)=>{return comment._id.toString()=== req.params.commentid});
+        if(!comment)
+        {
+            return res.status(404).json("Comment not Found");
+        }
+
+        if(comment.likes.filter((like)=>{return like.user.toString()===req.user.id}).length === 0)
+        {
+            return res.status(404).json("Comment Not Liked");
+        }
+        const removeIndex=comment.likes.map((like)=> like.user.toString()).indexOf(req.user.id);
+        
+        comment.likes.splice(removeIndex,1);
+        post.save();
+        return res.json(comment.likes);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json('Server Error');
+    }
+})
 module.exports=router
