2fb425f8e4cb921a92e404e1619416912216380b
Shivam Sharma <34922980+shivam4786@users.noreply.github.com>
2020-06-19 13:59:14 +0530

    Deployment

--- src/server.js
+++ src/server.js
@@ -39,7 +39,17 @@ if(process.env.NODE_ENV === 'production'){
     })
 }
 
-const PORT=process.env.PORT || 4000
+//serve static assests in production
+
+if(process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'devgram', 'build', 'index.html'))
+    })
+}
+
+const PORT = process.env.PORT || 4000
 
 app.listen(PORT,()=>{
     console.log(`Server Started on port ${PORT}`)
